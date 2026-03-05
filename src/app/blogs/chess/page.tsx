ea8fae0f6b5542b225a6b45098358d9838b5b978
chritzadz <chrisdumanauw@gmail.com>
2026-03-05 14:24:52 +0800

    chess page

---
+++ src/app/blogs/chess/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import ArticleSection from "@/components/ArticleSection";
+
+export default function ChessGame() {
+    return (
+        <div className="" style={{ fontFamily: 'Consolas, Times, serif' }}>
+            <h1 className="text-6xl font-bold">Chess Game</h1>
+            <div className="mt-8">
+                <ArticleSection id="intro" title="Introduction">
+                    <p className="mt-4">
+                        This is a simple page for the Chess game. You can expand this page to include rules, strategies, or even an interactive chess board in the future.
+                    </p>
+                </ArticleSection>
+            </div>
+        </div>
+    );
+}
